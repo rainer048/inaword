92f7e90175598fa704728c71921b24d9647e520c
rainer048 <rainer048@gmail.com>
2018-04-09 21:34:49 +0300

    Codestyle & security

--- 06_Lesson_6.sol
+++ 06_Lesson_6.sol
@@ -5,7 +5,7 @@ contract ERC20Basic {
 
     uint256 public totalSupply;
 
-    function balanceOf(address who) public constant returns (uint256);
+    function balanceOf(address who) public view returns (uint256);
     function transfer(address to, uint256 value) public returns (bool);
 
     event Transfer(address indexed from, address indexed to, uint256 value);
@@ -15,7 +15,7 @@ contract ERC20Basic {
 
 contract ERC20 is ERC20Basic {
 
-    function allowance(address owner, address spender) public constant returns (uint256);
+    function allowance(address owner, address spender) public view returns (uint256);
     function transferFrom(address from, address to, uint256 value) public returns (bool);
     function approve(address spender, uint256 value) public returns (bool);
 
